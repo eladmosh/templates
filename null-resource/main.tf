b3046fb2b92d39a0c36c02e67fcdb00a01c17b8a
eladmoshe <93523957+eladmosh@users.noreply.github.com>
2021-12-28 16:30:56 +0200

    Update main.tf

--- null-resource/main.tf
+++ null-resource/main.tf
@@ -3,3 +3,12 @@ resource "null_resource" "null1" {
 
 resource "null_resource" "null2" {
 }
+
+variable "dwarf" {
+    type        = string
+    description = "my little dwarf"
+}
+
+output "dwarf" {
+  value = var.dwarf
+}
