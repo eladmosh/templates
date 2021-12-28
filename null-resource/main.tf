f4e77c6bc2f4299506cab12309310d13496f5a06
eladmoshe <93523957+eladmosh@users.noreply.github.com>
2021-12-28 16:36:17 +0200

    Update main.tf

--- null-resource/main.tf
+++ null-resource/main.tf
@@ -4,7 +4,7 @@ resource "null_resource" "null1" {
 resource "null_resource" "null2" {
 }
 
-variable "dwarf" {
+variable "DWARF" {
     type        = string
     default = "Oin"
     description = "my little dwarf"
