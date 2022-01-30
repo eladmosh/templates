d8ed1ceb0fb1e12c45453d9f7bb39a48c371f17f
eladmoshe <93523957+eladmosh@users.noreply.github.com>
2022-01-30 17:47:19 +0200

    Create cluster.tf

---
+++ aws/k8s/cluster.tf
@@ -0,0 +1,7 @@
+module "my-cluster" {
+  source = "github.com/env0/k8s-modules//aws?ref=chore-add-modules-params"
+  #source = "github.com/env0/k8s-modules//aws"
+
+  region       = var.region
+  cluster_name = var.cluster_name
+}
