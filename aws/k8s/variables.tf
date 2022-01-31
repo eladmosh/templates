ce5245a5b7cba14a97cbd24eca0dabbf4d1f4a37
eladmoshe <93523957+eladmosh@users.noreply.github.com>
2022-01-31 23:34:52 +0200

    Update variables.tf

--- aws/k8s/variables.tf
+++ aws/k8s/variables.tf
@@ -2,3 +2,9 @@ variable "cluster_name" {}
 variable "region" {}
 variable "env0_values" {}
 variable "customer_values" {}
+variable "vpc" {
+  default = {
+    create = true
+    vpc_id = ""
+  }
+}
