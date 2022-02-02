2e0a09f4b116d799a100c993b39adbd85639985a
eladmoshe <93523957+eladmosh@users.noreply.github.com>
2022-02-02 17:45:25 +0200

    Update outputs.tf

--- aws/eks/outputs.tf
+++ aws/eks/outputs.tf
@@ -1,9 +1,9 @@
 output "kubernetes_host" {
-  value       = module.my_eks.eks.cluster_endpoint
+  value       = module.my_eks.kubernetes_host
   description = "EKS cluster host endpoint"
 }
 
 output "cluster_id" {
-  value       = module.my_eks.eks.cluster_id
+  value       = module.my_eks.cluster_id
   description = "EKS cluster id"
 }
