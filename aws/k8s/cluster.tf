de442f99b68e577ae2a469a95fa57dc60afd125c
eladmoshe <93523957+eladmosh@users.noreply.github.com>
2022-02-06 00:47:29 +0200

    Update cluster.tf

--- aws/k8s/cluster.tf
+++ aws/k8s/cluster.tf
@@ -2,8 +2,9 @@ module "my-cluster" {
   source = "github.com/env0/k8s-modules//aws?ref=chore-add-modules-params"
   #source = "github.com/env0/k8s-modules//aws"
 
-  region       = var.region
-  cluster_name = var.cluster_name
-  vpc          = var.vpc
-  eks          = var.eks
+  region            = var.region
+  cluster_name      = var.cluster_name
+  vpc               = var.vpc
+  eks               = var.eks
+  create_autoscaler = var.create_autoscaler
 }
