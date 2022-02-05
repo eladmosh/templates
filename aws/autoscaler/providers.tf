0e80c826ac7126805dd9a5f2fe6cb6de3533ce9f
eladmoshe <93523957+eladmosh@users.noreply.github.com>
2022-02-05 21:09:02 +0200

    Create providers.tf

---
+++ aws/autoscaler/providers.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_eks_cluster" "this" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = var.cluster_name
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.this.endpoint
+    token                  = data.aws_eks_cluster_auth.this.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
+  }
+}
