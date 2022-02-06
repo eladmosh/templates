f5e3871c956c4ea606cba0bab29c2a7dc03188a2
eladmoshe <93523957+eladmosh@users.noreply.github.com>
2022-02-06 15:15:38 +0200

    Update variables.tf

--- aws/k8s/variables.tf
+++ aws/k8s/variables.tf
@@ -80,34 +80,4 @@ variable "modules_info" {
       create = true
     }
   }
-
-  validation {
-    condition = !(var.modules_info.vpc.create == false && (var.modules_info.vpc.id == "" || length(var.modules_info.vpc.private_subnets) == 0))
-    error_message = "You must specify vpc_id if and private_subnets you don't want the vpc to be created."
-  }
-
-  validation {
-    condition = !(var.modules_info.eks.create == false && var.modules_info.eks.cluster_id == "")
-    error_message = "You must specify cluster_id if you don't want the eks to be created."
-  }
-
-  validation {
-    condition = !(!var.modules_info.eks.create && var.modules_info.vpc.create)
-    error_message = "You can't provision eks without vpc."
-  }
-
-  validation {
-    condition = !(!var.modules_info.efs.create && var.modules_info.efs.create)
-    error_message = "You can't proviosn efs without eks."
-  }
-
-  validation {
-    condition = !(!var.modules_info.autoscaler.create && var.modules_info.eks.create)
-    error_message = "You can't proviosn autoscaler without eks."
-  }
-
-  validation {
-    condition = !(!var.modules_info.csi_driver.create && var.modules_info.efs.create)
-    error_message = "You can't proviosn csi_driver without efs."
-  }
 }
