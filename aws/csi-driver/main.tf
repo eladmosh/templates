899a2492b57e8242b7d4ee0675c4321b5ed453f8
eladmoshe <93523957+eladmosh@users.noreply.github.com>
2022-02-06 11:22:50 +0200

    Update main.tf

--- aws/csi-driver/main.tf
+++ aws/csi-driver/main.tf
@@ -1,5 +1,5 @@
-module "my_autoscaler" {
-  source = "github.com/env0/k8s-modules//aws/autoscaler?ref=chore-add-modules-params"
+module "my_csi-driver" {
+  source = "github.com/env0/k8s-modules//aws/csi-driver?ref=chore-add-modules-params"
 
   cluster_name    = var.cluster_name
   efs_id          = var.efs_id
