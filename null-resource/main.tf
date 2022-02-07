a3f9811fde65dfcd1fc840bf6ff2147f83904519
GiliFaroEnv0 <gili.faro@env0.com>
2022-02-07 11:53:59 +0200

    add versions

--- null-resource/main.tf
+++ null-resource/main.tf
@@ -1,2 +1,20 @@
+terraform {
+  required_version = "99525.99.4"
+  required_providers {
+    archive = {
+      source = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    github = {
+      source = "integrations/github"
+      version = "~> 4.19.0"
+    }
+  }
+}
+
 resource "null_resource" "null" {
 }
