b0807a120b00d7b7e4138d87aa24f1953400a181
eladmoshe <93523957+eladmosh@users.noreply.github.com>
2022-06-22 10:21:51 +0300

    Create aws.tf

---
+++ terragrunt/modules/aws/aws.tf
@@ -0,0 +1,14 @@
+# aws.tf
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags {
+    Name        = "My bucket"
+  }
+}
