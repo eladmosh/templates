e5d03d1d51f568cc96f197d8767996cfe28bfcf1
eladmoshe <93523957+eladmosh@users.noreply.github.com>
2022-07-24 19:02:43 +0300

    Update main.tf

--- terragrunt/aws/main.tf
+++ terragrunt/aws/main.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "full_of_holes3" {
-  bucket = "my-tg-cost-estimations-test-bucket"
+resource "aws_s3_bucket" "full_of_holes4" {
+  bucket = "my-tg-cost-estimations-test-bucket4"
   acl    = "private"
 
   tags = {
-    Name        = "My bucket3"
+    Name        = "My bucket4"
   }
 }
