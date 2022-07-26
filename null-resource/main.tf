9721fa170bb813648cada52c96dbb633ae7ef3e3
eladmoshe <93523957+eladmosh@users.noreply.github.com>
2022-07-26 11:48:47 +0300

    Update main.tf

--- null-resource/main.tf
+++ null-resource/main.tf
@@ -4,6 +4,12 @@ resource "null_resource" "null1" {
 resource "null_resource" "null3" {
 }
 
+resource "random_string" "random" {
+  length           = 16
+  special          = true
+  override_special = "/@£$"
+}
+
 variable "DWARF" {
     type        = string
     default = "Oin"
