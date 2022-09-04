4f18290753924ccdf2bb77f1ff68a5125eba14c9
eladmoshe <93523957+eladmosh@users.noreply.github.com>
2022-09-04 12:08:28 +0300

    Update main.tf

--- null-resource/main.tf
+++ null-resource/main.tf
@@ -5,12 +5,6 @@ resource "null_resource" "null1" {
 resource "null_resource" "null3" {
 }
 
-resource "random_string" "random" {
-  length           = 16
-  special          = true
-  override_special = var.sensitive_var
-}
-
 variable "DWARF" {
     type        = string
     default = "Oin"
