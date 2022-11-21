abee3ffea18b9c060c915e029ccaac81c573e032
eladmoshe <93523957+eladmosh@users.noreply.github.com>
2022-11-21 15:59:01 +0200

    Update main.tf

--- module-registry/main.tf
+++ module-registry/main.tf
@@ -1,6 +1,6 @@
 module "sagi-module" {
   source = "api-dev.dev.env0.com/bd12a04a-b051-4f51-9a8a-8a32ac198802/sagi-module/gh"
-  version = "v0.0.1"
+  version = "v0.0.1234"
 }
 
 resource null_resource "bla" {
